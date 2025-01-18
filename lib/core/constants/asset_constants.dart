fda7f68ca383b4fe044810e8a1cfc3ab4413bb1b
Adwaith Jayasankar <kichu31084@gmail.com>
2025-01-18 16:08:05 +0530

    Add launcher icons and configuration for Android and iOS

--- lib/core/constants/asset_constants.dart
+++ lib/core/constants/asset_constants.dart
@@ -1,4 +1,5 @@
 class AssetConstants {
   static const String appLogo = 'assets/images/app_logo.png';
   static const String googleLogo = 'assets/images/google_logo.png';
+  static const String appIcon = 'assets/icons/app_icon.png';
 } 
